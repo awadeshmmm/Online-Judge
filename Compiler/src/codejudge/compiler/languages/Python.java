7106ba55eb702606fa170f2c14f71922ee1f90fe
awadesh kumar <awadesh22kumar@gmail.com>
2015-09-17 22:31:42 +0530

    Update Python.java

--- Compiler/src/codejudge/compiler/languages/Python.java
+++ Compiler/src/codejudge/compiler/languages/Python.java
@@ -1,10 +1,4 @@
-/*
- * Codejudge
- * Copyright 2012, Sankha Narayan Guria (sankha93@gmail.com)
- * Licensed under MIT License.
- *
- * Codejudge Compiler Server: Compiler for the Python language
- */
+
 
 package codejudge.compiler.languages;
 
