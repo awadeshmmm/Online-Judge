eece0b3220a93b890f240721220fc50c58bd3b11
awadesh kumar <awadesh22kumar@gmail.com>
2015-09-17 22:31:02 +0530

    Update Language.java

--- Compiler/src/codejudge/compiler/languages/Language.java
+++ Compiler/src/codejudge/compiler/languages/Language.java
@@ -1,10 +1,4 @@
-/*
- * Codejudge
- * Copyright 2012, Sankha Narayan Guria (sankha93@gmail.com)
- * Licensed under MIT License.
- *
- * Codejudge Compiler Server: The base interface for each language
- */
+
  
 package codejudge.compiler.languages;
 
