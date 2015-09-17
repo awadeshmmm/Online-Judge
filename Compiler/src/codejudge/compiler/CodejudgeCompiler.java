1007cb593239fa4e78267938e52f274938fefd5c
awadesh kumar <awadesh22kumar@gmail.com>
2015-09-17 22:27:26 +0530

    Update CodejudgeCompiler.java

--- Compiler/src/codejudge/compiler/CodejudgeCompiler.java
+++ Compiler/src/codejudge/compiler/CodejudgeCompiler.java
@@ -1,10 +1,4 @@
-/*
- * Codejudge
- * Copyright 2012, Sankha Narayan Guria (sankha93@gmail.com)
- * Licensed under MIT License.
- *
- * Codejudge Compiler Server
- */
+
  
 package codejudge.compiler;
 
